6dd91fd84e3b76b2232240af3932e2525fc96300
Emil <kilomil@hotmail.dk>
2017-04-22 12:29:07 +0200

    First map draft

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -7,6 +7,49 @@ public class MapController : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
+        CreateMap();
+    }
+
+    private void CreateMap()
+    {
+        Tiles = new[]
+        {
+           new [] {
+               CreateRoadTile(RoadSectionType.RoadCornerTopLeft),
+               CreateRoadTile(RoadSectionType.RoadHorizontal),
+               CreateRoadTile(RoadSectionType.RoadBranchDown),
+               CreateRoadTile(RoadSectionType.RoadHorizontal),
+               CreateRoadTile(RoadSectionType.RoadCornerTopRight),
+           },
+           new [] {
+               CreateRoadTile(RoadSectionType.RoadVertical),
+               null,
+               CreateRoadTile(RoadSectionType.RoadVertical),
+               null,
+               CreateRoadTile(RoadSectionType.RoadVertical),
+           },
+           new [] {
+               CreateRoadTile(RoadSectionType.RoadBranchRight),
+               CreateRoadTile(RoadSectionType.RoadHorizontal),
+               CreateRoadTile(RoadSectionType.RoadCross),
+               CreateRoadTile(RoadSectionType.RoadHorizontal),
+               CreateRoadTile(RoadSectionType.RoadBranchLeft),
+           },
+           new [] {
+               CreateRoadTile(RoadSectionType.RoadVertical),
+               null,
+               CreateRoadTile(RoadSectionType.RoadVertical),
+               null,
+               CreateRoadTile(RoadSectionType.RoadVertical),
+           },
+           new [] {
+               CreateRoadTile(RoadSectionType.RoadCornerBottomLeft),
+               CreateRoadTile(RoadSectionType.RoadHorizontal),
+               CreateRoadTile(RoadSectionType.RoadBranchUp),
+               CreateRoadTile(RoadSectionType.RoadHorizontal),
+               CreateRoadTile(RoadSectionType.RoadCornerBottomRight),
+           },
+        };
     }
 
     // Update is called once per frame
