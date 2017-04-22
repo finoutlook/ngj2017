d587aef61ca8afec75342cc035cd0378339ad251
Asbjørn Hansen <ah@fenris.io>
2017-04-22 03:02:32 +0200

    add road tile prefab and road segment prototype

---
+++ Assets/RoadSectionScript.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using UnityEngine;
+
+public class RoadSectionScript : MonoBehaviour
+{
+    public string Section;
+
+    private GameObject[] tiles;
+
+    private void CreateSection()
+    {
+        tiles = new GameObject[Section.Length];
+        for (var i = 0; i < Section.Length; i++)
+        {
+            var sectionType = Section[i];
+            RoadScript.RoadSectionType type;
+            switch (sectionType)
+            {
+                case 's':
+                    type = RoadScript.RoadSectionType.Straight;
+                    break;
+                case 'c':
+                    type = RoadScript.RoadSectionType.Curve;
+                    break;
+                case 'd':
+                    type = RoadScript.RoadSectionType.DeadEnd;
+                    break;
+                case 't':
+                    type = RoadScript.RoadSectionType.ThreeWayIntersection;
+                    break;
+                case 'x':
+                    type = RoadScript.RoadSectionType.FourWayIntersection;
+                    break;
+                case 'l':
+                    type = RoadScript.RoadSectionType.StraightLake;
+                    break;
+                case 'r':
+                    type = RoadScript.RoadSectionType.StraightRiver;
+                    break;
+                case 'C':
+                    type = RoadScript.RoadSectionType.StraightCanal;
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException(string.Format("Section type {0} not mapped.", sectionType));
+            }
+
+            var tile = (GameObject)Instantiate(Resources.Load("Prefabs/RoadTile"));
+            Debug.Log(tile.transform.position);
+            tile.transform.parent = transform;
+            Debug.Log(tile.transform.position);
+            //tile.transform.localPosition = Vector3.zero;
+            //tile.transform.position = new Vector2(0, i - 1);
+            Debug.Log(tile.transform.position);
+            tile.GetComponent<RoadScript>().SectionType = type;
+
+            tiles[i] = tile;
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        CreateSection();
+    }
+}
---
+++ Assets/Scripts/RoadScript.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RoadScript : MonoBehaviour
+{
+    public RoadSectionType SectionType;
+
+    [Header("Sprites")]
+    public Sprite Curve;
+
+    public Sprite DeadEnd;
+
+    public Sprite FourWayIntersection;
+
+    public Sprite Straight;
+
+    public Sprite StraightCanal;
+
+    public Sprite StraightLake;
+
+    public Sprite StraightRiver;
+
+    public Sprite ThreeWayIntersection;
+
+    private RoadSectionType currentSectionType;
+
+    public enum RoadSectionType
+    {
+        Straight = 0,
+
+        Curve = 1,
+
+        DeadEnd = 2,
+
+        ThreeWayIntersection = 3,
+
+        FourWayIntersection = 4,
+
+        StraightLake = 5,
+
+        StraightRiver = 6,
+
+        StraightCanal = 7
+    }
+
+    public Sprite GetSprite(RoadSectionType sectionType)
+    {
+        switch (sectionType)
+        {
+            case RoadSectionType.Curve:
+                return Curve;
+            case RoadSectionType.DeadEnd:
+                return DeadEnd;
+            case RoadSectionType.Straight:
+                return Straight;
+            case RoadSectionType.ThreeWayIntersection:
+                return ThreeWayIntersection;
+            case RoadSectionType.FourWayIntersection:
+                return FourWayIntersection;
+            case RoadSectionType.StraightLake:
+                return StraightLake;
+            case RoadSectionType.StraightRiver:
+                return StraightRiver;
+            case RoadSectionType.StraightCanal:
+                return StraightCanal;
+            default:
+                throw new InvalidEnumArgumentException(string.Format("Type {0} is not mapped to a sprite.", sectionType));
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        UpdateSprite();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (currentSectionType != SectionType)
+        {
+            UpdateSprite();
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        var selectedSprite = GetSprite(SectionType);
+
+        GetComponent<SpriteRenderer>().sprite = selectedSprite;
+        currentSectionType = SectionType;
+    }
+}
