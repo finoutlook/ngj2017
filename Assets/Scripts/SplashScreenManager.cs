9c8f658b844a6be9c5afe6aba915ff7ee76d2cc4
finoutlook <finoutlook@gmail.com>
2017-04-22 12:46:20 +0100

    Scenes for title and menus

---
+++ Assets/Scripts/SplashScreenManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreenManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Wait");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(4);
+        Application.LoadLevel("Start");
+    }
+}
