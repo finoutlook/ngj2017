6fa04a98d3f3a82b9458b34a379cebd2fb2a3f3e
Keith Lia <keithlia@gmail.com>
2017-04-22 14:44:09 +0200

    Re-added the ClueDescription because it was a stupid thing to remove it. Also added IsDecorationTile boolean

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -5,4 +5,6 @@ public class TileManager : MonoBehaviour
     public int TileId;
     public bool IsRoad;
     public bool IsPrize;
+    public bool IsDecorationTile;
+    public string ClueDescription;
 }
