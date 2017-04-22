ce63a4c34d6e6032cfee8bc0c65c3b058fb749d9
Keith Lia <keithlia@gmail.com>
2017-04-22 11:28:21 +0200

    Added script for GUI text

---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChanger : MonoBehaviour
+{
+    public Text uiText;
+    public float fadeDuration = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Text(string text)
+    {
+        if (!string.IsNullOrEmpty(uiText.text))
+        {
+            uiText.canvasRenderer.SetAlpha(0.1f);
+        }
+
+        uiText.text = text;
+        uiText.CrossFadeAlpha(1f, fadeDuration, false);
+    }
+}
