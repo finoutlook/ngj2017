773f4094025bd1bea45f945ad312db231e4e2d86
Asbjørn Hansen <ash@moviestarplanet.com>
2017-04-22 11:52:59 +0200

    change how road tiles are drawn

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject[][] Tiles;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
