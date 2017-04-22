cc29a08b295e2e18d7c15d359829893cef7e1b34
Emil <kilomil@hotmail.dk>
2017-04-22 20:19:41 +0200

    Text effect when changing clue

--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TextChanger : MonoBehaviour
 {
     public Text uiText;
-    public float fadeDuration = 1f;
+    public float fadeDuration = 0.3f;
 
+    public string currentText;
+    private bool fadeCurrent;
     public void Text(string text)
     {
         if (!string.IsNullOrEmpty(uiText.text))
         {
-            uiText.canvasRenderer.SetAlpha(0.1f);
+            fadeCurrent = true;
+           
         }
 
-        uiText.text = text;
-        uiText.CrossFadeAlpha(1f, fadeDuration, false);
+        currentText = text;
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        if(fadeCurrent)
+        {
+            fadeCurrent = false;
+            uiText.CrossFadeAlpha(0f, fadeDuration, false);
+            yield return new WaitForSeconds(fadeDuration);
+        }
+        for (int i = 0; i < (currentText.Length + 1); i++)
+        {
+            uiText.CrossFadeAlpha(1f, 0, true);
+            uiText.text = currentText.Substring(0, i);
+            yield return new WaitForSeconds(.03f);
+        }
     }
 }
