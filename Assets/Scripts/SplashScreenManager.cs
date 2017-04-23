d1315e5271704fff58b5ea92d90eb2ca13c80aed
finoutlook <finoutlook@yahoo.co.uk>
2017-04-23 09:53:34 +0100

    Play only a section of the dial-up sound on the splash screen

--- Assets/Scripts/SplashScreenManager.cs
+++ Assets/Scripts/SplashScreenManager.cs
@@ -2,21 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SplashScreenManager : MonoBehaviour {
-
+public class SplashScreenManager : MonoBehaviour
+{
 	// Use this for initialization
 	void Start () {
         StartCoroutine("Wait");
+
+        AudioSource audioSource = GameObject.FindObjectOfType<AudioSource>();
+        audioSource.time = 5.00f;
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        AudioSource audioSource = GameObject.FindObjectOfType<AudioSource>();
+
+        if (audioSource.time > 7.55f)
+        {
+            audioSource.Stop();
+        }
+    }
 
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(5);
         Application.LoadLevel("Start");
     }
 }
